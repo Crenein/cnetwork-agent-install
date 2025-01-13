6cfc5d1c8b8583e1d7fe960ec5d660aa9a9359b7
Nicolas Paz Almirón <94925477+PazNicolas@users.noreply.github.com>
2025-01-13 15:17:22 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -77,7 +77,7 @@ services:
       - mongo-data:/data/db
 
   cnetwork-agent:
-    image: crenein/c-network-agent:0.5.6
+    image: crenein/c-network-agent:0.6.5
     container_name: cnetwork-agent
     ports:
       - "8000:8000"

e4d6f1f8dd3acc1945bb4bcff777ff7c28d8da43
facubertran <facubertran@gmail.com>
2025-02-21 15:53:11 -0300

    Update install_mongodb4.sh
    0.9.8

--- install_mongodb4.sh
+++ install_mongodb4.sh
@@ -89,7 +89,7 @@ services:
       - app-network
 
   cnetwork-agent:
-    image: crenein/c-network-agent:0.6.9
+    image: crenein/c-network-agent:0.9.8
     container_name: cnetwork-agent
     ports:
       - "8000:8000"

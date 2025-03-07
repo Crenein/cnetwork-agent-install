9626c287932b87b24650ec675149838dc39e3bcb
Nicolas Paz Almirón <94925477+PazNicolas@users.noreply.github.com>
2025-03-07 19:23:36 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -117,7 +117,7 @@ services:
       - app-network
 
   cnetwork-agent:
-    image: crenein/c-network-agent:0.9.9
+    image: crenein/c-network-agent:0.1.0
     container_name: cnetwork-agent
     ports:
       - "8000:8000"
